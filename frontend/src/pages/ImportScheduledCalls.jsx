f1818816f2164932528433e6317f28bfc6a3ce33
tanmaykhambhadiya12 <tanmmaykhambhadiya@gmail.com>
2025-11-15 12:57:59 +0530

    backend call logs complete

--- frontend/src/pages/ImportScheduledCalls.jsx
+++
@@ -1,9 +0,0 @@
-import { ExcelImport } from '../components/ExcelImport';
-
-export const ImportScheduledCalls = () => {
-  return (
-    <div>
-      <ExcelImport onDataReady={() => console.log('Data ready')} />
-    </div>
-  );
-};
